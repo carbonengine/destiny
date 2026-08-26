76df020b56d9edbc534b0e77f8ed7dc7fc5572b4
yuqian19870725-creator <yuqian19870725@gmail.com>
2026-08-26 17:42:32 +0800

    Remove redundant partition key hash test

--- tests/TestPartition.cpp
+++ tests/TestPartition.cpp
@@ -11,34 +11,3 @@ TEST(PartitionKeyHash, SeparatesCoordinatesThatPreviouslyCollided)
     // (1 << 3) + 16 == (2 << 2) + 16.
     EXPECT_NE(KeyHash{}(first), KeyHash{}(second));
 }
-
-TEST(PartitionKeyHash, RetainsEveryDistinctBoxKey)
-{
-    MapOfBoxes boxes;
-    constexpr int gridSize = 16;
-
-    for (int64_t ix = 0; ix < gridSize; ++ix)
-    {
-        for (int64_t iy = 0; iy < gridSize; ++iy)
-        {
-            for (int64_t iz = 0; iz < gridSize; ++iz)
-            {
-                boxes.emplace(Key(ix, iy, iz, gridSize), nullptr);
-            }
-        }
-    }
-
-    EXPECT_EQ(boxes.size(), static_cast<size_t>(gridSize * gridSize * gridSize));
-
-    for (int64_t ix = 0; ix < gridSize; ++ix)
-    {
-        for (int64_t iy = 0; iy < gridSize; ++iy)
-        {
-            for (int64_t iz = 0; iz < gridSize; ++iz)
-            {
-                const auto found = boxes.find(Key(ix, iy, iz, gridSize));
-                ASSERT_NE(found, boxes.end());
-            }
-        }
-    }
-}
